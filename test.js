18b384339312b3e2856e62ea27b6b98a4409470e
Sinu Mohan <sinumohan777@gmail.com>
2019-06-23 14:38:05 +0530

    Key from env

--- test.js
+++ test.js
@@ -2,9 +2,8 @@ var assert = require('assert');
 var Clockify = require('./app.js');
 var expect = require('expect.js');
 const fs = require('fs');
-var Key = fs.readFileSync('./clockify.key');
 it('Clockify.Workspaces.get() Should return an array of Workspaces', (done) => {
-    Clockify.SetKey(Key);
+    Clockify.SetKey(process.env.CLOCKIFY_API_KEY);
     const resolvingPromise = Clockify.Workspaces.get();
     resolvingPromise.then( (result) => {
         expect(result).to.be.an('array');
